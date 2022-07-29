d59aaad677b540bfe4cf4cc3505b7055a5fcaa46
Lautaro Desouches <lautarodesouches@outlook.com>
2022-07-29 00:07:34 -0300

    [ProductDetail] everything but colors

--- src/screens/ProductDetail/index.tsx
+++ src/screens/ProductDetail/index.tsx
@@ -1,8 +1,10 @@
 import { useState } from 'react';
-import { Image, Text, View } from 'react-native'
-import { PriceInfo, Thumnail } from '../../components';
-import { PRODUCT_IMAGE_URL } from '../../constants/Urls';
+import { Image, ScrollView, Text, TextInput, TouchableOpacity, View } from 'react-native'
+import { PriceInfo, Thumnail } from '../../components'
+import { PRODUCT_IMAGE_URL } from '../../constants/Urls'
 import { styles } from './styles'
+import Ionicons from '@expo/vector-icons/Ionicons'
+import { primaryBg } from '../../constants/Colors/index';
 
 const ProductDetailScreen = ({ route, navigation }: { route: any, navigation: any, item: any }) => {
 
@@ -11,18 +13,47 @@ const ProductDetailScreen = ({ route, navigation }: { route: any, navigation: an
     const { id, name, brand, category, price, discount, sold, opinions, stars, amountAvailable, freeShipping, availableImages, availableColors, description } = item
 
     const [numberMainImage, setNumberMainImage] = useState(1)
+    const [showSelectQty, setShowSelectQty] = useState(false)
+    const [quantity, setQuantity] = useState(1)
+    const [quantityInputValue, setQuantityInputValue] = useState('')
+    const [amountAvailableWarn, setAmountAvailableWarn] = useState(false)
+
+    const quantityText = (quantity: number) => `${quantity} unidad${quantity > 1 ? 'es' : ''}`
+
+    const QuantityOption = ({ option }: { option: number }) => (
+        <TouchableOpacity style={styles.optionContainer} onPress={() => handleQuantitySelected(option)}>
+            <Text style={styles.optionText}>{option}</Text>
+        </TouchableOpacity>
+    )
+
+    const handleQuantitySelected = (number: number) => {
+        setShowSelectQty(false)
+        if (!number) return
+        let lessThanAmountAvailable = amountAvailable < number
+        setAmountAvailableWarn(lessThanAmountAvailable)
+        setQuantity(lessThanAmountAvailable ? amountAvailable : number)
+    }
 
     return (
-        <View style={styles.container}>
+        <ScrollView style={styles.container}>
             <View style={styles.productContainer}>
+                <View style={styles.nav}>
+                    <TouchableOpacity onPress={() => console.log(category)}>
+                        <Text style={styles.navText}>{category}</Text>
+                    </TouchableOpacity>
+                    <Text style={styles.navText}>{' > '}</Text>
+                    <TouchableOpacity onPress={() => console.log(brand)}>
+                        <Text style={styles.navText}>{brand}</Text>
+                    </TouchableOpacity>
+                </View>
                 <View style={styles.imagesContainer}>
                     <View style={styles.mainImageContainer}>
                         <Image source={{ uri: `${PRODUCT_IMAGE_URL}${id}-${numberMainImage}.png` }} style={styles.mainImage} />
                     </View>
                     <View style={styles.thumnailImagesContainer}>
                         {
-                            [...Array(availableImages)].map((e, index) => (
-                                <Thumnail key={index} productId={id} imageNumber={index + 1} handleSelectImage={setNumberMainImage} numberMainImage={numberMainImage} />)
+                            [...Array(availableImages)].map((_, index) => (
+                                <Thumnail key={'thumnail' + index} productId={id} imageNumber={index + 1} handleSelectImage={setNumberMainImage} numberMainImage={numberMainImage} />)
                             )
                         }
                     </View>
@@ -33,8 +64,54 @@ const ProductDetailScreen = ({ route, navigation }: { route: any, navigation: an
                     freeShipping={freeShipping}
                     price={price}
                 />
+                <Text style={styles.description}>{description}</Text>
+                <View style={styles.statsContainer}>
+                    <Text style={styles.statText}>{sold} vendidos</Text>
+                    <Text style={styles.statText}>{opinions} opiniones</Text>
+                </View>
+                <View style={styles.stars}>
+                    {
+                        [...Array(5)].map((_, index) =>
+                            <Ionicons key={'star' + index} size={28} color='#cc9900' name={stars >= index + 1 ? 'star' : (stars <= index ? 'star-outline' : 'star-half-sharp')} />
+                        )
+                    }
+                </View>
+                <View style={styles.quantityContainer}>
+                    <TouchableOpacity style={styles.quantityTouchable} onPress={() => setShowSelectQty(!showSelectQty)}>
+                        <Text style={styles.actualQuantity}>Cantidad seleccionada: {quantityText(quantity)}</Text>
+                        <Ionicons size={22} color={primaryBg} name={showSelectQty ? 'chevron-up-circle' : 'chevron-down-circle'} />
+                    </TouchableOpacity>
+                    <Text style={styles.amountAvailable}>Disponible: {quantityText(amountAvailable)}</Text>
+                    {
+                        showSelectQty && (
+                            <View style={styles.selectQuantity}>
+                                {
+                                    [...Array(4)].map((_, index) => <QuantityOption key={'option' + index} option={index + 1} />)
+                                }
+                                <View style={styles.optionContainer}>
+                                    <TextInput
+                                        placeholder='5 o más'
+                                        keyboardType='number-pad'
+                                        style={styles.optionText}
+                                        value={quantityInputValue}
+                                        textContentType='telephoneNumber'
+                                        onFocus={() => setQuantityInputValue('5')}
+                                        onChangeText={value => setQuantityInputValue(value)}
+                                        onEndEditing={() => handleQuantitySelected(parseInt(quantityInputValue))}
+                                    />
+                                </View>
+                            </View>
+                        )
+                    }
+                    {
+                        amountAvailableWarn && (
+                            <Text style={styles.inputWarn}>Solo hay {amountAvailable} unidades disponibles</Text>
+                        )
+                    }
+                </View>
+                <Text>Colores disponibles:</Text>
             </View>
-        </View>
+        </ScrollView>
     )
 }
 
--- src/screens/ProductDetail/styles.tsx
+++ src/screens/ProductDetail/styles.tsx
@@ -5,13 +5,20 @@ export const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: themeBg,
-        padding: 10
+        padding: 10,
     },
     productContainer: {
         flex: 1,
         backgroundColor: '#fff',
         padding: 10,
-        borderRadius: 5
+        borderRadius: 5,
+        paddingBottom: 150
+    },
+    nav: {
+        flexDirection: 'row'
+    },
+    navText: {
+        fontSize: 16
     },
     imagesContainer: {
         height: 250,
@@ -41,9 +48,71 @@ export const styles = StyleSheet.create({
         padding: 5
     },
     title: {
-        fontSize: 20,
+        fontSize: 22,
         marginTop: 20,
         marginBottom: 10,
+        textAlign: 'center'
+    },
+    description: {
+        fontSize: 18,
+        marginTop: 20
+    },
+    statsContainer: {
+        marginTop: 20,
+        flexDirection: 'row',
+        justifyContent: 'space-evenly'
+    },
+    statText: {
+        fontSize: 18
+    },
+    stars: {
+        width: '80%',
+        alignSelf: 'center',
+        flexDirection: 'row',
+        justifyContent: 'space-evenly',
+        marginTop: 20
+    },
+    quantityContainer: {
+        marginVertical: 30,
+    },
+    quantityTouchable: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'flex-end'
+    },
+    actualQuantity: {
+        textAlign: 'center',
+        fontSize: 18,
+        marginRight: 5
+    },
+    amountAvailable: {
+        marginTop: 10,
+        textAlign: 'center',
+        fontSize: 17
+    },
+    selectQuantity: {
+        alignSelf: 'center',
+        width: '50%',
+        borderRadius: 5,
+        borderWidth: 1,
+        position: 'relative',
+        marginTop: 10,
+        zIndex: 99
+    },
+    optionContainer: {
+        paddingVertical: 5,
+        borderColor: '#e1e1e1',
+        borderWidth: 1,
+        backgroundColor: '#fff',
+    },
+    optionText: {
+        fontSize: 18,
+        textAlign: 'center'
+    },
+    inputWarn: {
+        marginTop: 15,
+        color: 'crimson',
+        fontSize: 15,
         textAlign: 'center'
     }
 })
