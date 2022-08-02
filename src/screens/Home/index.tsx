78606ecc8638192ea71f44bcf98164b774971c8a
Lautaro Desouches <lautarodesouches@outlook.com>
2022-08-02 16:45:32 -0300

    Firebase

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -2,8 +2,30 @@ import { FlatList, View } from 'react-native'
 import { ProductPreview } from '../../components'
 import { styles } from './styles'
 import { useSelector } from 'react-redux'
+import db from '../../utils/firebaseConfig'
+import { collection, getDocs, query } from "firebase/firestore"
+import { useEffect } from 'react'
 
 const HomeScreen = ({ navigation }: { navigation: any }) => {
+
+    useEffect(() => {
+        (async function () {
+            const querySnapshot = query(collection(db, "products"))
+            return await getDocs(querySnapshot);
+        })()
+            .then((result) => {
+
+                if (!result.docs) throw new TypeError("Hubo un error por favor intente mas tarde");
+
+                console.log(
+                    result.docs.map(doc => ({ id: doc.id, ...doc.data() }))
+                )
+
+            })
+            .catch(error => {
+                console.log(error)
+            })
+    }, [])
 
     const products = useSelector((state: any) => state.product.products)
 
