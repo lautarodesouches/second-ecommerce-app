2f87bc50975c96ca7918dd836b2277c099da3faf
Lautaro Desouches <lautarodesouches@outlook.com>
2022-08-04 11:37:55 -0300

    Custom alert fade animation

--- src/components/CustomAlert/index.tsx
+++ src/components/CustomAlert/index.tsx
@@ -1,12 +1,30 @@
-import { Modal, Text, View } from 'react-native'
+import { Animated, Modal, Text, View } from 'react-native'
 import CloseButton from '../Buttons/Close'
 import ButtonDanger from '../Buttons/Danger'
 import { styles } from './styles'
 import ButtonMuted from '../Buttons/Muted/index'
+import { useRef } from 'react'
 
 const CustomAlert = ({ setShowAlert, optionAction, message, option }: { setShowAlert: any, optionAction: any, message: string, option: string }) => {
 
-    const closeModal = () => setShowAlert(false)
+    const fadeAnim = useRef(new Animated.Value(1)).current
+
+    const closeModal = () => {
+
+        Animated.timing(
+            fadeAnim,
+            {
+                toValue: 0,
+                duration: 500,
+                useNativeDriver: true
+            }
+        ).start()
+
+        setTimeout(() => {
+            setShowAlert(false)
+        }, 501)
+
+    }
 
     const handlePress = () => {
         optionAction()
@@ -18,14 +36,14 @@ const CustomAlert = ({ setShowAlert, optionAction, message, option }: { setShowA
             transparent
             onRequestClose={closeModal}
         >
-            <View style={styles.container}>
+            <Animated.View style={[styles.container, { opacity: fadeAnim }]}>
                 <View style={styles.alert}>
                     <CloseButton onPress={closeModal} />
                     <Text style={styles.text}>{message}</Text>
                     <ButtonMuted onPress={closeModal} title='Cancelar' />
                     <ButtonDanger onPress={handlePress} title={option} />
                 </View>
-            </View>
+            </Animated.View>
         </Modal>
     )
 }
